d3f757ff6d4d9182fc1d207a358071c1f71c6dd0
AnneRom <coolann000.com@gmail.com>
2024-10-01 18:36:59 +0300

    Lesson_01-10
    Зробили стилі для materials__content, додали до хедер можливість ховатись при скролінгу.

--- ecoCabins/script.js
+++ ecoCabins/script.js
@@ -1,21 +1,27 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
+// const swiper = new Swiper('.swiper', {
+    
+//     loop: true,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//   });
+
+  console.log("Hello!")
+
+let lastScrollTop = 0; //змінна для збереження позиції останнього скролінгу
+const header = document.querySelector('.header');
+
+console.log(header);
+
+window.addEventListener ('scroll', () => {
+    let scrollTop = window.scrollY || document.documentElement.scrollTop;//поточна позиція скролінгу
+    if ((scrollTop > lastScrollTop) && (scrollTop > header.offsetHeight)) {
+        header.classList.add('hidden');
+    } else if (scrollTop < lastScrollTop) {
+        header.classList.remove('hidden');
+    }
+
+    lastScrollTop = scrollTop;
+});
